c72f0507bfff0da65017886c868f889669f7686f
JTL (Saman) <15827947+joeythelantern@users.noreply.github.com>
2021-01-05 20:11:32 -0500

    Update user.ts

--- source/models/user.ts
+++ source/models/user.ts
@@ -3,8 +3,8 @@ import IUser from '../interfaces/user';
 
 const UserSchema: Schema = new Schema(
     {
-        title: { type: String, required: true },
-        author: { type: String, required: true }
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
     },
     {
         timestamps: true
